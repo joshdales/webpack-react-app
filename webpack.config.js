d5d27ccbe6318efacd44af5531e79334b2671298
Josh Dales <joshdales@pm.me>
2021-03-06 17:15:23 -0500

    Get webpack woeking with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  devtool: 'eval-cheap-module-source-map',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    extensions: ['.js', 'jsx', 'ts', 'tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: '/.tsx$/',
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+              "@babel/preset-react", {
+                "runtime": "automatic"
+              }
+            ]
+          }
+        }
+      }
+    ]
+  }
 }
