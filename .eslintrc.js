4777ff149bdb36782015217d5a6f0d1cb582e8c7
Josh Dales <joshdales@pm.me>
2021-03-06 18:40:08 -0500

    Add react eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
   plugins: [
     '@typescript-eslint',
+    'react',
+    'react-hooks'
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'prettier'
   ],
 }
