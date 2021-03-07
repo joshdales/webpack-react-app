0845f754efebbc4d25a3658e87818837753c7f36
Josh Dales <joshdales@pm.me>
2021-03-07 09:19:08 -0500

    Update webpack for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,42 @@ const path = require('path')
 const webpack = require('webpack')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
+const inDevMode = process.env.NODE_ENV === 'development'
+
+/**
+ * Our entry points to our code
+ */
+let entry = [
+  './src/index.tsx'
+]
+
+/**
+ * The sourcemap (or lack there of that we are going to be using)
+ */
+let devtool = false
+
+/**
+ * The webpack plugins that we are going to be using
+ */
+let plugins = []
+
+if (inDevMode) {
+  devtool = 'eval-cheap-module-source-map'
+  entry = entry.concat('webpack-hot-middleware/client')
+  plugins = plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockIntegration: 'whm'
+      }
+    })
+  ])
+}
+
 module.exports = {
-  mode: "development",
-  devtool: 'eval-cheap-module-source-map',
-  entry: [
-    './src/index.tsx',
-    'webpack-hot-middleware/client'
-  ],
+  mode: inDevMode ? "development" : "production",
+  devtool,
+  entry,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -20,14 +49,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|ts)x?$/,
+        test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            plugins: [
+            plugins: inDevMode ? [
               'react-refresh/babel'
-            ],
+            ] : undefined,
             presets: [
               "@babel/preset-env",
               "@babel/preset-typescript",
@@ -38,12 +67,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin({
-      overlay: {
-        sockIntegration: 'whm'
-      }
-    })
-  ]
+  plugins
 }
