2c7693a152508127bcd458051ed915da00c6e8e0
Josh Dales <joshdales@pm.me>
2021-03-06 20:26:25 -0500

    Yay the server works now - well it serves the index at least

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const PORT = 3000
+const path = require('path')
 
 const express = require('express')
+const app = express()
+
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 
-const app = express()
 const config = require('./webpack.config')
 const compiler = webpack(config)
-
-app.use(webpackDevMiddleware(compiler, {
+const compilerOptions = {
   publicPath: config.output.publicPath
-}))
+}
 
+app.use(webpackDevMiddleware(compiler, compilerOptions))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+const PORT = 3000
 app.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
