ee2ad94a9b9ac8b954e1b936f55759016a4af49c
Josh Dales <joshdales@pm.me>
2021-03-08 08:36:55 -0500

    Adjust the public path for local and production

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/",
+    publicPath: inDevMode ? "/" : './',
     clean: true
   },
   resolve: {
