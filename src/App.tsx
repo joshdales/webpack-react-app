74fec2797ea8e05f7ac2b1271de4c109af801ca9
Josh Dales <joshdales@pm.me>
2021-03-08 18:52:01 -0500

    Add some basic styles and content to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react'
 
+import './App.css'
+
 function App(): JSX.Element {
   return (
-    <main>
-      <h1>Hi there!</h1>
-    </main>
+    <>
+      <main>
+        <h1>This is a basic React app</h1>
+
+        <p>It's for quickly creating a Typescript based react app.</p>
+
+        <p>
+          It doesn't do anything fancy, it's just an easy setup for getting
+          started with some tools like eslint and prettier.
+        </p>
+
+        <p>And as an added bonus you can easily deploy it to Github Pages</p>
+      </main>
+
+      <footer>
+        <p>
+          Made with the{' '}
+          <a href="https://github.com/joshdales/react-app-base">
+            react-app-base
+          </a>{' '}
+          template
+        </p>
+      </footer>
+    </>
   )
 }
 
